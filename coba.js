7169d4ac886cb2b3a029bdb90663401bb690a62b
dave-andreas <57472457+dave-andreas@users.noreply.github.com>
2020-03-04 11:49:20 +0700

    asd

---
+++ coba.js
@@ -0,0 +1,23 @@
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+  
+  // Used like so
+  var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29];
+  shuffle(arr);
+  console.log(arr);
